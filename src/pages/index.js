9c813a5187120ea27166c24740748c25b5048dfe
Hurrell <cem.hurrell@gmail.com>
2020-09-28 18:40:36 +0100

    Add simple project to front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import "./index.css"
+
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
+import PP1 from "../images/Patchpal_screenshot_1.png"
+import PP2 from "../images/Patchpal_screenshot_2.png"
+import PP3 from "../images/Patchpal_screenshot_3.png"
+
+import { graphql } from "gatsby"
 
 const IndexPage = () => (
   <Layout currentPage="Projects">
@@ -16,6 +23,30 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
+    <div className="project">
+      <div className="project-images">
+        <img className="project-image" src={PP1} alt="" />
+        <img className="project-image" src={PP2} alt="" />
+        <img className="project-image" src={PP3} alt="" />
+      </div>
+      <div className="project-details">
+        <h3>
+          <i>PatchPal</i>
+        </h3>
+        <p>Design, Specification and build.</p>
+        <p>
+          A mobile-first web app that allows easy navigation around a database
+          of event lighting equipment. Users can look up key facts, and add the
+          lights to a list to quickly find their total weight and power draw.
+        </p>
+        <p> React | JS | HTML | CSS</p>
+        <p>
+          <a href="https://www.patchpalapp.com">www.patchpalapp.com</a>
+
+          <a href="https://www.github.com/Hurrell/patchpalapp">Github</a>
+        </p>
+      </div>
+    </div>
   </Layout>
 )
 
